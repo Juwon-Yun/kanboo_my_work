05fbc026459ba09ec1e8ee735251aa1441791882
Juwon-Yun <eooll2@daum.net>
2021-12-12 12:10:48 +0900

    add filter

--- src/components/component/acess/projectDetail/scheduler/Filter.vue
+++ src/components/component/acess/projectDetail/scheduler/Filter.vue
@@ -2,31 +2,31 @@
    <h1>Filter</h1>
     <ul>
       <li> 
-        <input type="checkbox" id="all" @click="selectedCheckbox">
+        <input checked type="checkbox" class="filter-wrap" id="all" @click="selectedCheckbox">
         <label for="all">전체</label>
       </li>
       <li> 
-        <input type="checkbox" id="common" @click="selectedCheckbox">
+        <input type="checkbox" class="filter-wrap" id="common" @click="selectedCheckbox">
         <label for="common">공통</label>
       </li>
       <li> 
-        <input type="checkbox" id="individual" @click="selectedCheckbox">
+        <input type="checkbox" class="filter-wrap" id="individual" @click="selectedCheckbox">
         <label for="individual">개인</label>
       </li>
       <li> 
-        <input type="checkbox" id="notice" @click="selectedCheckbox">
+        <input type="checkbox" class="filter-wrap" id="notice" @click="selectedCheckbox">
         <label for="notice">공지</label>
       </li>
       <li> 
-        <input type="checkbox" id="Emergency" @click="selectedCheckbox">
+        <input type="checkbox" class="filter-wrap" id="Emergency" @click="selectedCheckbox">
         <label for="Emergency">긴급</label>
       </li>
       <li> 
-        <input type="checkbox" id="vacation" @click="selectedCheckbox">
+        <input type="checkbox" class="filter-wrap" id="vacation" @click="selectedCheckbox">
         <label for="vacation">휴가</label>
       </li>
       <li> 
-        <input type="checkbox" id="note" @click="selectedCheckbox">
+        <input type="checkbox" class="filter-wrap" id="note" @click="selectedCheckbox">
         <label for="note">기타</label>
       </li>
     </ul>
@@ -34,13 +34,34 @@
 
 <script>
 import { mapMutations } from 'vuex'
+
 export default {
+  data() {
+    return {
+      filterFlag : false,
+    }
+  },
   methods : {
     ...mapMutations({
       setFilterValue : 'scheduler/setFilterValue',
     }),
     selectedCheckbox(e){
-      console.log(e.target.id)
+      if(e.type === 'click'){
+        const chkBox = document.querySelectorAll('.filter-wrap')
+        for(let i = 0; i < chkBox.length; i++){
+          if(!this.filterFlag){
+            if(e.target.id !== chkBox[i].id){
+              chkBox[i].checked = false
+            }
+            this.filterFlag = !this.filterFlag
+          }else if(this.filterFlag){
+            if(chkBox[i].id === 'all'){
+              chkBox[i].checked = true
+            }
+            this.filterFlag = !this.filterFlag
+          }
+        }
+      }
       this.setFilterValue(e.target.id)
     },
   },
